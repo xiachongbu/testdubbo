0fd9acaf1fa24cfbaf767cd7f4e7e34721e4d605
xiachongbu <597134636@qq.com>
2018-06-17 14:36:33 +0800

    设置了负载均衡机制与失败切换机制【consumer】，以及线程池的配置【provider】

--- test-provider-message/src/main/java/testdubbo/provider/service/impl/MessageServiceImpl.java
+++ test-provider-message/src/main/java/testdubbo/provider/service/impl/MessageServiceImpl.java
@@ -11,8 +11,8 @@ public class MessageServiceImpl implements IMessageService {
     @Override
     public Message echo(Message msg) {
         Message echoMsg = new Message();
-        echoMsg.setTitle("【ECHO】" + msg.getTitle());
-        echoMsg.setContent("【ECHO】" + msg.getContent());
+        echoMsg.setTitle("【ECHO - " + Thread.currentThread().getName() + "】" + msg.getTitle());
+        echoMsg.setContent("【ECHO - " + Thread.currentThread().getName() + "】" + msg.getContent());
         echoMsg.setSender("MessageProvider");
         return echoMsg;
     }
